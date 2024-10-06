f600c36ea33046659e4be2b76f419b29f1748146
MangoSama1000 <tilija_nitej@hotmail.com>
2024-10-06 17:27:45 +0100

    project

---
+++ src/app/components/AuthForm.jsx
@@ -0,0 +1,26 @@
+'use client'
+
+import {Auth} from '@supabase/auth-ui-react';
+import {createClientComponentClient} from '@supabase/auth-helpers-nextjs'
+
+export default function AuthForm(){
+    const supabase = createClientComponentClient();
+    return (
+        <Auth
+        supabaseClient={supabase}
+        view="magic_link"
+        showLinks={false}
+        providers={[]}
+        redirectTo='http://localhose:300/auth/callback'
+        appearance={{
+            them:'dark',
+            button:{
+                className: 'bg-white-0400 text-gray-900 hover:bg-gray-600'
+            },
+            input:{
+                className: 'bg-gray-700 border-gray-600 text-white'
+            }
+        }}
+        />
+    )
+}
